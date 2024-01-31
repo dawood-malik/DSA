945972cbbd971c90ec68170d8e0ebda31c6ac664
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:52:27 +0530

    Leet 724

---
+++ e724/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 7, 3, 6, 5, 6 }; //o: 3
+            int sum_left = 0;
+            int sum_right = 0;
+            foreach (int x in nums)
+            {
+                sum_right += x;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum_left += nums[i];
+                if (sum_right == sum_left)
+                {
+                    Console.WriteLine(i);
+                }
+                sum_right -= nums[i];
+            }
+        }
+    }
+}
