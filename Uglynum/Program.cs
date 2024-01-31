d4563c412d2dac1e6ac9e9869efa31c06715871d
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:13:29 +0530

    Ugly Number

---
+++ Uglynum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml;
+
+namespace leo 
+{   
+    class BinaryNode
+    {
+        public int data;
+        public BinaryNode left;
+        public BinaryNode right;
+    }
+
+    class BinaryTree
+    {
+        
+    }
+
+    class Program
+    {
+       
+        public static void Main(string[] args) 
+        {
+           
+            
+        }
+
+    }
+
+}
