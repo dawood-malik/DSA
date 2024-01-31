3d70e788aa18eef3f9aefce8ef6773763d77f915
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:53:44 +0530

    Leet 605

---
+++ e605/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace kuu
+{
+    class Program 
+    { 
+        static void Main(string[] args)
+        {
+            int[] flowerbed = { 1, 0, 0, 0, 1 };
+            int n = 2;
+            //Output: true
+            int k=flowerbed.Length;
+
+            int count = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                if (flowerbed[i] == 0 && ( i == 0 || flowerbed[i - 1] == 0) &&( i == k - 1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+                
+            }
+            bool check = count >= n;
+            Console.WriteLine(check);
+        }
+    }
+
+}
