35fc645bc464e2fc66fc79c2354afbf6581e561e
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:21:34 +0530

    Leet 1679

---
+++ m1679/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace kuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 }; int k = 5;
+            //Output: 2
+            List<int> list = new List<int>(nums);
+            Array.Clear(nums);
+            list.Sort();
+            nums =list.ToArray();
+
+            
+            int i = 0;
+            int j = nums.Length-1;
+            int count = 0;
+
+            while (i <= j)
+            {
+                int sum = 0;
+                sum = nums[i] + nums[j];
+                if (sum < k)
+                {
+                    i++;
+                }
+                else if (sum > k)
+                {
+                    j--;
+                }
+                else
+                {
+                    list.Remove(nums[i]);
+                    list.Remove(nums[j]);
+                    i++;
+                    j--;
+                    count++;
+                }
+            }
+            
+            
+        }
+    }
+}
