09615d4f50f445b3eef613db1d5f0649494d7d7f
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:11:47 +0530

    Binary Tree

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Leo
+{
+    class BinaryNode
+    {
+        public int data;
+        public BinaryNode left;
+        public BinaryNode right;
+    }
+
+    class BinaryTree
+    {
+        public BinaryNode root = new BinaryNode();
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void AddNode(int data)
+        {
+            root = AddRecursive(root, data);
+        }
+        public BinaryNode AddRecursive(BinaryNode node, int data)
+        {
+            if (node == null)
+            {
+                node = new BinaryNode();
+                node.data = data;
+            }
+            else if (data < node.data)
+            {
+                node.left = AddRecursive(node.left, data);
+            }
+            else if (data > node.data)
+            {
+                node.right = AddRecursive(node.right, data);
+            }
+
+            return node;
+        }
+        public void preOrderTraversal(BinaryNode node)
+        {
+            if (node != null)
+            {
+                Console.WriteLine(node.data);
+                preOrderTraversal(node.left);
+                preOrderTraversal(node.right);
+            }
+
+        }
+
+
+    }
+
+
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.AddNode(5);
+            binaryTree.AddNode(3);
+            binaryTree.AddNode(7);
+            binaryTree.AddNode(1);
+            binaryTree.AddNode(4);
+            binaryTree.AddNode(6);
+            binaryTree.AddNode(8);
+
+            binaryTree.preOrderTraversal(binaryTree.root);
+
+
+        }
+
+    }
+
+}
