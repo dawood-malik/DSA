40d691e371407dd2d8724fac89f43eda86231e05
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:47:53 +0530

    Leet 10 Hard

---
+++ h10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace kuu 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3 };
+            int n = nums.Length - 1;
+            int index = -1;
+
+            for(int i=n-2;i>=0;i--)
+            {
+                if (nums[i] < nums[i + 1])  //2<3 index=1;
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if(index == -1)
+            {
+                Array.Reverse(nums);
+                return;
+            }
+
+            for(int i = n - 1; i > index; i++)
+            {
+                if (nums[i] > nums[index])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[index];
+                    nums[index] = temp;
+                    break;
+                }
+            }
+            Array.Sort(nums, index + 1, n - 1 - index);
+        }
+    }
+
+}
