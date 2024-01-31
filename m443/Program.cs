dc817f7c3d147b4e1c33b77267dcc3b42a5a73da
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:32:46 +0530

    Leet 443

---
+++ m443/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace kuuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
+            int i = 0;
+            int res = 0;
+            
+            while(i < chars.Length)
+            {
+                int add = 1;
+                while (i+add<chars.Length && chars[i + add] == chars[i])
+                {
+                    add++;
+                }
+                chars[res] = chars[i];
+                res++;
+
+                if (add > 1)
+                {
+                    foreach(var val in add.ToString().ToCharArray())
+                    {
+                        chars[res]=val;
+                        res++;
+                    }
+                }
+                i += add; 
+            }
+            for(int j= 0;j< res; j++)
+            {
+                Console.WriteLine(chars[j]);    
+            }
+        }
+    }
+}
