a8eb83b44e5f68d06b22a6998084365c4ddb7b90
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:56:42 +0530

    Leet 345

---
+++ e345/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Kuuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello";
+            string helper = "aeiouAEIOU";
+            Stack<char> stack = new Stack<char>();
+            StringBuilder sb = new StringBuilder();
+            foreach(var k in str)
+            {
+                if (helper.Contains(k))
+                {
+                    stack.Push(k);
+                }
+            }
+            foreach(var k in str)
+            {
+                if(helper.Contains(k))
+                {
+                    sb.Append(stack.Pop()); 
+                }
+                else
+                {
+                    sb.Append(k);
+                }
+            }
+            Console.WriteLine(sb.ToString());
+                
+        }
+    }
+}
