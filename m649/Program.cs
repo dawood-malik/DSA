70c56d8f007f583548e83db2ca35a251ea75425b
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:31:23 +0530

    Leet 649

---
+++ m649/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace kuu
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string senate = "RD";
+            Queue<char> queue = new Queue<char>(senate);
+            int countR = senate.Count(x => x == 'R');
+            int countD = senate.Length - countR;
+            int scale = 0;
+            while(countD > 0 && countR>0)
+            {
+                char senator=queue.Dequeue();
+                if (senator == 'R')
+                {
+                    if (scale >= 0)
+                    {
+                        countD--;
+                        queue.Enqueue(senator);
+                    }
+                    scale++;
+                }
+                else
+                {
+                    if(scale >= 0)
+                    {
+                        countR--;
+                        queue.Enqueue(senator);
+                    }
+                    scale--;
+                }
+            }
+
+        }
+        
+
+    }
+
+}
