7e25cf06d21fb952c635e421ef10b3d32f81de19
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:55:13 +0530

    Leet 392

---
+++ e392/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace kuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "acb";
+            string t = "ahbgdc";
+            int i = 0;
+            int j = 0;
+            while (i < s.Length && j < t.Length)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+                j++;
+            }
+            Console.WriteLine(i == s.Length);
+
+
+        }
+    }
+}
