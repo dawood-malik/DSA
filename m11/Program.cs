8efe41ebdce7c80bdefebe86126ac3c543d98181
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:44:45 +0530

    Leet 11

---
+++ m11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace kuuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            //Output: 49
+            int MaxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+            while (left < right)
+            {
+                int h = Math.Min(height[left], height[right]);
+                int breadth = right - left;
+                MaxArea=Math.Max(MaxArea,h*breadth);
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            Console.WriteLine(MaxArea);
+            
+        }
+    }
+}
