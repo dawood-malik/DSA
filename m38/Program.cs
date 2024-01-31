22ca4a8250868a628b0c0f7ecf44136aade507ca
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:43:12 +0530

    Leet 38

---
+++ m38/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace kuuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val = "1";
+            int n = 5;
+            string helper = "";
+
+            if (n == 0)
+            {
+                helper = "";
+            }
+            
+            for (int i=0;i < n;i++)
+            {
+                if (i == 0)
+                {
+                    helper = val;
+                }
+                else
+                {
+                    helper = formsequence(helper);
+                    
+                }   
+
+            }
+            Console.WriteLine(helper);
+        }
+
+        public static string formsequence(string val)
+        {
+            char[] chars= val.ToCharArray();
+            string newseq = "";
+            char current = chars[0];
+            int count = 1;
+
+            for(int i = 1; i < chars.Length; i++)
+            {
+                if (chars[i]==current)
+                {
+                    count++;
+                }
+                else
+                {
+                    newseq += count.ToString() + current;
+                    count = 1;
+                    current = chars[i];
+                }
+            }
+            newseq += count.ToString() +current;
+
+            return newseq;
+        }
+    }
+}
