45060228a65bde8e28d233ab3f0c5c6a5ee14b4c
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:16:29 +0530

    Leet 2390 Code

---
+++ m2390/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "leet**cod*e";
+            var newstring = new StringBuilder();
+            
+            for(int i=0;i<s.Length; i++)
+            {
+                if (s[i] == '*')
+                {
+                    newstring.Length--;
+                }
+                else
+                {
+                    newstring.Append(s[i]);
+                }
+            }
+            Console.WriteLine(newstring.ToString());
+        }
+    }
+}
