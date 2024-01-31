6e46027ca1ec19bc3304d013e827b9ba6ed383b3
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:25:01 +0530

    Leet 1456

---
+++ m1456/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aeiou"; int k = 2;
+            //Output: 3
+            int vowelInWindow = 0;
+            int max = 0;
+            string vowels = "aeiouAEIOU";
+            for (int i = 0; i< k; i++) 
+            {
+                if (vowels.Contains(s[i]))
+                {
+                    vowelInWindow++;
+                }
+            }
+            max=vowelInWindow;
+
+            for(int i=k; i < s.Length; i++)
+            {
+                if(vowels.Contains(s[i-k]))
+                {
+                    vowelInWindow--;
+                }
+                if (vowels.Contains(s[i]))
+                {
+                    vowelInWindow++;
+                }
+                if (vowelInWindow > max)
+                {
+                    max = vowelInWindow;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
