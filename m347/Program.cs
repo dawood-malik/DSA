f877002b22d4881e826c56bac87d2c07342821f3
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:34:02 +0530

    Leet 347

---
+++ m347/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace kuuu
+{
+    class Program 
+    {
+        //Input: nums = [1,1,1,2,2,3], k = 2
+        //Output: [1,2]
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 1, 2, 2, 3 };
+            int k = 2;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            foreach(var num in nums)
+            {
+                if (!map.ContainsKey(num))
+                {
+                    map.Add(num, 0);
+                }
+                map[num]++;
+            }
+            var sorted_dict=map.OrderByDescending(x=>x.Value).ToList();
+            IList<int> list = new List<int>();
+            foreach(var num in sorted_dict)
+            {
+                Console.WriteLine(num.Key+"="+num.Value);
+            }
+            
+            
+        }   
+
+    }
+
+}
