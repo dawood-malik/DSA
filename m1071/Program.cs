39b725d32e3b61bc2ca0a656e98d716c82c8fe65
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:26:48 +0530

    Leet 1071

---
+++ m1071/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace kuuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "ABCDEF";
+            string str2 = "ABC";
+            int maxlength = 0;
+            int index = 0;
+            int actual = 0;
+            int[,] dp=new int[str1.Length+1,str2.Length+1];
+
+            for(int i=1; i<=str1.Length; i++)
+            {
+                for(int j=1; j<=str2.Length; j++)
+                {
+                    if (str1[i - 1] == str2[j - 1])
+                    {
+                        dp[i, j] = dp[i-1,j-1]+1;
+                        if (dp[i, j] >= 1)
+                        {
+                            maxlength = dp[i, j];
+
+                            
+                        }
+                        
+                        
+                    }
+                    else
+                    {
+                        dp[i, j] = 0;
+                    }
+                }
+            }
+            
+
+        }
+    
+    }
+}
