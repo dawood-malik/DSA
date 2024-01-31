6bd13a205fdebeec301f5d4db9ee4e9910928f0e
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:35:46 +0530

    Leet 334

---
+++ m334/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace kuuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int first = int.MaxValue;
+            int second = int.MaxValue;
+            bool check = false;
+            foreach(var num in nums)
+            {
+                if (num < first)
+                {
+                    first = num;
+                }
+                else if (num < second)
+                {
+                    second = num;
+                }
+                else
+                {
+                    check= true;
+                }
+            }
+            Console.WriteLine(check);
+            
+            
+            
+                
+        }
+    }
+}
