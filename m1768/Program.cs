a4ccccaa5cbd3e0834e37847baf009a5cc182fc9
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:20:23 +0530

    Leet 1768

---
+++ m1768/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace kuuu
+{
+    class Program 
+    { 
+        static void Main(string[] args)
+        {
+            string word1 = "abc";
+            string word2 = "pqrst";
+            int left = 0;
+            int i = 0;
+            int j = 0;
+            int len=word1.Length+word2.Length-1;
+
+            while (left<=len)
+            {
+                if (i < word1.Length)
+                {
+                    Console.Write(word1[i]);
+                    i++;
+                }
+                if(j< word2.Length)
+                {
+                    Console.Write(word2[j]);
+                    j++;
+                }
+                left++;
+            }
+        }
+    }
+
+}
