13b1d06af6278aa2c8db4ae24fdb361992260303
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:59:07 +0530

    Leet 121

---
+++ e121/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace kuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            //Output: 5
+            int left = 0;
+            int right = prices.Length-1;
+            int maxval = 0;
+
+            while (left < right)
+            {
+                int val = prices[right] - prices[left];
+                Console.WriteLine(val);
+                maxval = Math.Max(maxval, val);
+                if (prices[left] < prices[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+            Console.WriteLine(maxval);
+        }
+    }
+}
