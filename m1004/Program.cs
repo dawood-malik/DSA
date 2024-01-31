13531a02a46cb759cb2fa64f4931583642bfa404
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:28:33 +0530

    Leet 1004

---
+++ m1004/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace coding
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }; int k = 2;
+            //Output: 6
+            int max_count = 0;
+            int left = 0;
+            for(int right = 0; right < nums.Length; right++)
+            {
+                k += nums[right] == 0 ? -1 : 0;
+                if (k < 0)
+                {
+                    k += nums[left] == 0 ? 1 : 0;
+                    left++;
+                }
+                else
+                {
+                    max_count = Math.Max(max_count, right - left+1);
+                }
+            }
+            Console.WriteLine(max_count);
+        }
+    }
+}
