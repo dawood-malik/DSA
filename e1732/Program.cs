f1fd316554c961943c2fe651965e5a19bd4e1bbc
dawood-malik <67229360+dawood-malik@users.noreply.github.com>
2024-01-31 11:49:55 +0530

    Leet 1732

---
+++ e1732/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] gain = { -5, 1, 5, 0, -7};
+            int[] altitude = new int[gain.Length+1];
+            altitude[0] = 0;
+            int temp = 0;
+            int max = int.MinValue;
+            max = Math.Max(altitude[0], max);
+            for (int i = 0; i < gain.Length; i++)
+            {
+                temp = altitude[i];
+                altitude[i + 1] = gain[i] + temp;
+                max = Math.Max(altitude[i+1], max); 
+            }
+            Console.Write(max);
+            
+        }
+    }
+}
